262830b74e90f0855a9d51f2a0946f7946ec60f8
Cristian Sepulveda <cristiangsc@gmail.com>
2022-06-23 19:43:31 -0400

    Desafio task

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div>
+      <h2>Crea una nueva tarea</h2>
+      <span>Tarea </span>
+      <input type="text" v-model="task" @keyup.enter="addTask" placeholder="Ingrese una nueva tarea">
+      <button @click="addTask">Crear</button>
+      <Task :tareas= "tareas" @eliminacion="eliminar" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Task from "@/components/Task";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {Task},
+  data() {
+    return {
+      task: '',
+      tareas: []
+    }
+  },
+  methods: {
+    addTask() {
+      if (this.task !== '') {
+        this.tareas.push(this.task)
+        this.task = ''
+      }
+    },
+    eliminar(index) {
+      console.log(index)
+      this.tareas.splice(index, 1)
+    }
   }
 }
 </script>
@@ -19,10 +41,28 @@ export default {
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
 }
+
+.container {
+  display: flex;
+  justify-content: center;
+}
+
+span {
+  font-weight: bold;
+}
+
+button {
+  margin-left: 5px;
+}
+
+li {
+  margin-left: 30%;
+  text-align: justify;
+}
+
 </style>
---
+++ src/components/Task.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>Lista</h2>
+    <ul>
+      <li v-for="(tarea, index) in tareas" :key="index">
+        {{ index + 1 }}: {{ tarea }}
+        <span> <button @click="$emit('eliminacion',index)">Eliminar</button> </span>
+      </li>
+    </ul>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "Task",
+  props: {
+    tareas: [],
+  },
+
+}
+</script>
+
+<style>
+li {
+  padding-bottom: 10px;
+}
+</style>
